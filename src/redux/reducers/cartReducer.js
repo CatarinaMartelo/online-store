f7b1e52fd7da736627a9190300b55c0ef17c7c04
Catarina Martelo <catarina_martelo@hotmail.com>
2023-03-09 19:52:15 +0000

    local storage ok

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -1,35 +1,38 @@
-const savedCart = localStorage.getItem('cart');
-const cart = savedCart ? JSON.parse(savedCart) : [];
+const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
 
-const cartReducer = (state = cart, action) => {
+const cartReducer = (state = initialState, action) => {
   const product = action.payload;
 
   switch (action.type) {
     case "ADDITEM":
-        const existingProduct = state.find((x) => x.id === product.id);
-        if (existingProduct) {
-          return state;
-        } else {
-          return [...state, { ...product, qty: 1 }];
-        }
+      const existingProduct = state.find((x) => x.id === product.id);
+      if (existingProduct) {
+        return state;
+      } else {
+        const newState = [...state, { ...product, qty: 1 }];
+        localStorage.setItem('cart', JSON.stringify(newState));
+        return newState;
+      }
 
     case "DELITEM":
-        const updatedCart = state.map((x) =>
+      const updatedCart = state.map((x) =>
         x.id === product.id ? { ...x, qty: x.qty - 1 } : x
       ).filter((x) => x.qty !== 0);
+      localStorage.setItem('cart', JSON.stringify(updatedCart));
       return updatedCart;
 
-      case "CHANGE_CART_QTY":
-        return state.map((product) =>
-          product.id === action.payload.id
-            ? { ...product, qty: action.payload.qty }
-            : product
-        );
-    
+    case "CHANGE_CART_QTY":
+      const newState = state.map((product) =>
+        product.id === action.payload.id
+          ? { ...product, qty: action.payload.qty }
+          : product
+      );
+      localStorage.setItem('cart', JSON.stringify(newState));
+      return newState;
+
+    default:
+      return state;
   }
-  
-  localStorage.setItem('cart', JSON.stringify(state));
-  return state;
 };
 
 export default cartReducer;
